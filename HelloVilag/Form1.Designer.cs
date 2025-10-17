f899c30d2d1bca4537c8898ffbac36b15bf358ec
DESKTOP-9MT43A6\veger <vegeraj@gmail.com>
2025-10-17 09:56:15 +0200

    Add project files.

---
+++ HelloVilag/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace HelloVilag
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label_felirat = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label_felirat
+            // 
+            this.label_felirat.AutoSize = true;
+            this.label_felirat.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label_felirat.Location = new System.Drawing.Point(121, 53);
+            this.label_felirat.Name = "label_felirat";
+            this.label_felirat.Size = new System.Drawing.Size(130, 29);
+            this.label_felirat.TabIndex = 0;
+            this.label_felirat.Text = "Helló Világ";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(135, 101);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(101, 51);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Kattints ide";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(382, 200);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label_felirat);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Első program";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label_felirat;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ HelloVilag/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HelloVilag
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //A gombra kattintva jelenjen meg az előző labelen a következő szöveg: "Üdvözöllek!"
+            label_felirat.Text = "Üdvözöllek!";
+        }
+    }
+}
